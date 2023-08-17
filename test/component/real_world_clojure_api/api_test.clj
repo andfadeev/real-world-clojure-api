3a7247d2c57255cd5b3b06cde2c7dd304bf84460
Andrey Fadeev <andfadeev@gmail.com>
2023-08-17 22:52:43 +0100

    [PART-3] todo post api, content type and schema validation (#2)

--- test/component/real_world_clojure_api/api_test.clj
+++ test/component/real_world_clojure_api/api_test.clj
@@ -1,5 +1,6 @@
 (ns component.real-world-clojure-api.api-test
-  (:require [clojure.string :as str]
+  (:require [cheshire.core :as json]
+            [clojure.string :as str]
             [clojure.test :refer :all]
             [com.stuartsierra.component :as component]
             [real-world-clojure-api.components.pedestal-component :refer [url-for]]
@@ -32,35 +33,82 @@
     (is (= {:body "Hello world"
             :status 200}
            (-> (sut->url sut (url-for :greet))
-             (client/get)
+             (client/get {:accept :json})
              (select-keys [:body :status]))))))
 
+(deftest content-negotiation-test
+  (testing "only application/json is accepted"
+    (with-system
+      [sut (core/real-world-clojure-api-system {:server {:port (get-free-port)}})]
+      (is (= {:body "Not Acceptable"
+              :status 406}
+             (-> (sut->url sut (url-for :greet))
+                 (client/get {:accept :edn
+                              :throw-exceptions false})
+                 (select-keys [:body :status])))))))
+
 (deftest get-todo-test
-  (let [todo-id-1 (random-uuid)
+  (let [todo-id-1 (str (random-uuid))
         todo-1 {:id todo-id-1
                 :name "My todo for test"
-                :items [{:id (random-uuid)
+                :items [{:id (str (random-uuid))
                          :name "finish the test"}]}]
     (with-system
       [sut (core/real-world-clojure-api-system {:server {:port (get-free-port)}})]
       (reset! (-> sut :in-memory-state-component :state-atom)
               [todo-1])
-      (is (= {:body (pr-str todo-1)
+      (is (= {:body todo-1
               :status 200}
              (-> (sut->url sut
                            (url-for :get-todo
                                     {:path-params {:todo-id todo-id-1}}))
-                 (client/get)
+                 (client/get {:accept :json
+                              :as :json
+                              :throw-exceptions false})
                  (select-keys [:body :status]))))
       (testing "Empty body is return for random todo id"
         (is (= {:body ""
-                :status 200}
+                :status 404}
                (-> (sut->url sut
                              (url-for :get-todo
                                       {:path-params {:todo-id (random-uuid)}}))
-                   (client/get)
+                   (client/get {:throw-exceptions false})
                    (select-keys [:body :status]))
                ))))))
 
-(deftest a-simple-api-test
-  (is (= 1 1)))
+(deftest post-todo-test
+  (let [todo-id-1 (str (random-uuid))
+        todo-1 {:id todo-id-1
+                :name "My todo for test"
+                :items []}]
+    (with-system
+      [sut (core/real-world-clojure-api-system {:server {:port (get-free-port)}})]
+      (testing "store and retrieve todo by id"
+        (is (= {:body todo-1
+                :status 201}
+               (-> (sut->url sut (url-for :post-todo))
+                   (client/post {:accept :json
+                                 :content-type :json
+                                 :as :json
+                                 :throw-exceptions false
+                                 :body (json/encode todo-1)})
+                   (select-keys [:body :status]))))
+        (is (= {:body todo-1
+                :status 200}
+               (-> (sut->url sut
+                             (url-for :get-todo
+                                      {:path-params {:todo-id todo-id-1}}))
+                   (client/get {:accept :json
+                                :as :json
+                                :throw-exceptions false})
+                   (select-keys [:body :status])))))
+
+      (testing "invalid Todo is rejected"
+        (is (= {:status 500}
+               (-> (sut->url sut (url-for :post-todo))
+                   (client/post {:accept :json
+                                 :content-type :json
+                                 :as :json
+                                 :throw-exceptions false
+                                 :body (json/encode {:id todo-id-1})})
+                   (select-keys [:status]))))))))
